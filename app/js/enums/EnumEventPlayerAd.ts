4093db28af04000b7870555ce0ae1579a7d4c999
Gaina Dan <danugaina1403@gmail.com>
2022-09-21 18:46:59 +0300

    [change] rename event Stop to Pause

--- app/js/enums/EnumEventPlayerAd.ts
+++ app/js/enums/EnumEventPlayerAd.ts
@@ -1,7 +1,7 @@
 enum EnumEventPlayerAd {
     PlayPlayerAd = 'play-player-ad',
     PlayAdPlayerAd = 'play-ad-player-ad',
-    StopPlayerAd = 'stop-player-ad',
+    PausePlayerAd = 'pause-player-ad',
     MutePlayerAd = 'mute-player-ad',
     UnmutePlayerAd = 'unmute-player-ad',
     SkipAdPlayerAd = 'skip-ad-player-ad'
